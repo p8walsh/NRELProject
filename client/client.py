e85313205fdab7fa5ed63f578473287bed949fd2
Peter Walsh <peterbug80228@gmail.com>
2022-10-29 19:22:20 -0600

    More password authentication, error handling, and GUI changes

--- client/client.py
+++ client/client.py
@@ -1,29 +1,11 @@
 import socket
-import sys
-import time
-import threading
 from tkinter import *
 from tkinter import ttk
 from cryptography.hazmat.primitives import hashes
 
 import Encrypt
 
-# python3 rcmdTCP.py proteus8.ddns.net 12344 5 2 time
-
-def normalExecution(serverAddress, port, exeCount, delay, command, serverAddressPort):
-
-    if input() == "rcend":
-        print("User Input rcend Detected, stopping program.")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((serverAddress, port))
-            s.sendto("rcend".encode("Latin-1"), serverAddressPort)
-        mainThread.join()
-        sys.exit()
-    else:
-        mainThread.join()
-        sys.exit()
-
-def authenticate(password, socket, errormsg):
+def authenticate(password, socket, serverAddressPort, errormsg, auth_status, password_root):
     # First add salt to password for unique hashing
     password = password + "salt"
 
@@ -34,9 +16,56 @@ def authenticate(password, socket, errormsg):
     print(hashed_password)
 
     # Then transmit password
-    errormsg.set("Authentication Unsuccessful. Try again.")
+    socket.sendto(hashed_password, serverAddressPort)
+    msgFromServer, addr = socket.recvfrom(2048)
+    msgFromServer = msgFromServer.decode("Latin-1")
+    if msgFromServer == "Authentication Success! Executing command...":
+        errormsg.set("Authentication Success! Executing command...")
+        auth_status.set(True)
+        password_root.destroy()
+    else:
+        errormsg.set("Authentication Unsuccessful. Try again.")
+        auth_status.set(False)
+
+def execute_command(s, command, exeCount, delay, results, command_root):
+    if command == "stop":
+        command_root.destroy()
+        
+        # Encrypting the command
+        keyList = ["a","b","c","d"]
+        command = Encrypt.encrypt(keyList, command)
+        print("\n\nCommand after encryption:", command)
+
+        msgToServer = exeCount + ", " + delay + ", " + command
+        s.sendto(msgToServer.encode("Latin-1"), serverAddressPort)
+
+        exit(0)
+        
+    # Encrypting the command
+    keyList = ["a","b","c","d"]
+    command = Encrypt.encrypt(keyList, command)
+    print("\n\nCommand after encryption:", command)
+
+    try:
+        print("Sending command:\nhost:", serverAddress, "\nport:", port, "\nExecution Count:", exeCount, "\nDelay Time:", delay, "\nCommand:", command, "\n")
+
+        msgToServer = exeCount + ", " + delay + ", " + command
+        s.sendto(msgToServer.encode("Latin-1"), serverAddressPort)
+
+        for i in range(int(exeCount)):
+            print("Execution Number:", i)
+            msgFromServer, addr = s.recvfrom(2048)
+            print("Time at server:", msgFromServer.decode("Latin-1"))
+            msgFromServer, addr = s.recvfrom(2048)
+            results.set(msgFromServer.decode("Latin-1"))
+            print(results.get())
+
+
+    except Exception as e:
+        raise e
 
 if __name__ == '__main__':
+    # Establish connection to server
     root = Tk()
     frm = ttk.Frame(root, padding=10)
     frm.grid()
@@ -44,36 +73,24 @@ if __name__ == '__main__':
     #serverAddress = StringVar(value="proteus8.ddns.net")
     serverAddress = StringVar(value="PetersComputer")
     port = StringVar(value="12344")
-    exeCount = StringVar(value="5")
-    delay = StringVar(value="2")
-    command = StringVar(value="time")
+    
 
     ttk.Label(frm, text="Server Address: ").grid(column=0, row=0)
     ttk.Entry(frm, textvariable=serverAddress).grid(column=1, row=0)
     ttk.Label(frm, text="Port Number: ").grid(column=0, row=1)
     ttk.Entry(frm, textvariable=port).grid(column=1, row=1)
-    ttk.Label(frm, text="Command: ").grid(column=0, row=2)
-    ttk.Entry(frm, textvariable=command).grid(column=1, row=2)
-    ttk.Label(frm, text="Times to execute command: ").grid(column=0, row=3)
-    ttk.Entry(frm, textvariable=exeCount).grid(column=1, row=3)
-    ttk.Label(frm, text="Delay between executions: ").grid(column=0, row=4)
-    ttk.Entry(frm, textvariable=delay).grid(column=1, row=4)
-    ttk.Button(frm, text="Enter", command=root.destroy).grid(column=1, row=5)
+    ttk.Button(frm, text="Enter", command=root.destroy).grid(column=1, row=2)
     root.bind("<Return>", lambda e: root.destroy())
     root.mainloop()
 
     serverAddress = serverAddress.get()
     port = int(port.get())
-    exeCount = exeCount.get()
-    delay = delay.get()
-    command = command.get()
-
-    print(serverAddress, port, exeCount, delay, command)
 
     # Create connection to server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # Complete initial connection
         s.connect((serverAddress, port))
+        serverAddressPort = (serverAddress, port)
 
         # Authenticate user first:
         password_root = Tk()
@@ -81,42 +98,34 @@ if __name__ == '__main__':
         password_frm.grid()
         password = StringVar()
         errormsg = StringVar()
+        auth_status = BooleanVar(value=False)
         ttk.Label(password_frm, text="Password: ").grid(column=0, row=0)
         ttk.Entry(password_frm, textvariable=password, show="*").grid(column=1, row=0)
         ttk.Button(password_frm, text="Enter", command=password_root.destroy).grid(column=1, row=1)
         ttk.Label(password_frm, textvariable=errormsg).grid(column=1, row=2)
-        password_root.bind("<Return>", lambda e: authenticate(password.get(), s, errormsg))
+        password_root.bind("<Return>", lambda e: authenticate(password.get(), s, serverAddressPort, errormsg, auth_status, password_root))
 
         password_root.mainloop()
 
-        serverAddressPort = (serverAddress, port)
+        print(auth_status.get())
 
-        # Encrypting the command
-        keyList = ["a","b","c","d"]
-        command = Encrypt.encrypt(keyList, command)
-        print("\n\nCommand after encryption:", command)
-        
-        args = (serverAddress, port, exeCount, delay, command, serverAddressPort)
+        # Allow user to continue entering new commands
+        command_root = Tk()
+        command_frm = ttk.Frame(command_root, padding=10)
+        command_frm.grid()
+        results = StringVar()
+        exeCount = StringVar(value="5")
+        delay = StringVar(value="2")
+        command = StringVar(value="time")
+        ttk.Label(command_frm, text="Command: ").grid(column=0, row=0)
+        ttk.Entry(command_frm, textvariable=command).grid(column=1, row=0)
+        ttk.Label(command_frm, text="Times to execute command: ").grid(column=0, row=1)
+        ttk.Entry(command_frm, textvariable=exeCount).grid(column=1, row=1)
+        ttk.Label(command_frm, text="Delay between executions: ").grid(column=0, row=2)
+        ttk.Entry(command_frm, textvariable=delay).grid(column=1, row=2)
+        ttk.Button(command_frm, text="Enter", command=lambda: execute_command(s, command.get(), exeCount.get(), delay.get(), results, command_root)).grid(column=1, row=3)
+        ttk.Label(command_frm, textvariable=results).grid(column=1, row=4)
+        command_root.bind("<Return>", lambda e: execute_command(s, command.get(), exeCount.get(), delay.get(), results, command_root))
 
-        mainThread = threading.Thread(target=normalExecution, args=args)
-        mainThread.daemon = True
-        try:
-            mainThread.start()
-            print("Sending command:\nhost:", serverAddress, "\nport:", port, "\nExecution Count:", exeCount, "\nDelay Time:", delay, "\nCommand:", command, "\n")
-
-            msgToServer = exeCount + ", " + delay + ", " + command
-            s.sendto(msgToServer.encode("Latin-1"), serverAddressPort)
-
-            for i in range(int(exeCount)):
-                print("Execution Number:", i)
-                msgFromServer, addr = s.recvfrom(2048)
-                print("Time at server:", msgFromServer.decode("Latin-1"))
-                msgFromServer, addr = s.recvfrom(2048)
-                print(msgFromServer.decode("Latin-1"))
-
-
-        except Exception as e:
-            print(e)
-            mainThread.join()
-            raise e
+        command_root.mainloop()
 
--- server/server.py
+++ server/server.py
@@ -2,10 +2,18 @@ import socket
 import sys
 import time
 import subprocess
+from cryptography.hazmat.primitives import hashes
 
 import Encrypt
 
 X = 1024
+
+password = "password123" + "salt"
+keyList = ["a","b","c","d"]
+digest = hashes.Hash(hashes.SHA512())
+digest.update(password.encode('Latin-1'))
+hashed_password = digest.finalize()
+accepted_hashes = [hashed_password]
 
 def parseCommand(command):
     print("command: ", command)
@@ -36,7 +44,9 @@ else:
 print(host)
 s.bind((host, port))
 
+
 while True:
+    # Authenticate password   
     print("Waiting for data on port", port)
     s.listen()
     connection, address = s.accept()
@@ -44,34 +54,63 @@ while True:
     print("Connected to client!")
     try:
         with connection:
-            data = connection.recv(X).decode("Latin-1")
-            exeCount, delay, command = parseCommand(data)
+            auth_status = False
+            while auth_status == False:
+                data = connection.recv(X)
+                #print("Data: ", data)
+                #data = data.decode("Latin-1")
 
-            # Try decrypting the command
-            keyList = ["a","b","c","d"]
-            print("Command before decryption:", command)
-            command = Encrypt.encrypt(keyList, command)
-            print("\n\nCommand after decryption:", command)
+                # Decrypt input
+                #data = Encrypt.encrypt(keyList, data)
 
-            print("Received Command:", data)
+                # Compare hash with accepted password hashes
+                for hash in accepted_hashes:
+                    print(data, hash, data == hash)
+                    if data == hash:
+                        auth_status = True          
+                if auth_status == True:
+                    # Send password succeeded message
+                    connection.send(("Authentication Success! Executing command...").encode("Latin-1"))
+                else:
+                    # Send authentication failed message 
+                    connection.send(("Authentication Failed. Try again.").encode("Latin-1"))
 
-            for i in range(int(exeCount)):
-                print("Execution Number:", i)
-                startTime = time.time()
-                try:
-                    output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-                except subprocess.CalledProcessError as e:
-                    output = str(e).encode('Latin-1')
-                endTime = time.time()
-                print("Execution Time:", endTime-startTime, "second(s)")
-                print("Execution Output:", output.decode("Latin-1"))
-                print("\nOutput Size:", len(output), "bytes\n")
+            while True:
+                data = connection.recv(X).decode("Latin-1")
+                exeCount, delay, command = parseCommand(data)
 
-                serverTime = subprocess.check_output("date", stderr=subprocess.STDOUT, shell=True)
-                connection.send(serverTime)
-                connection.send(output)
+                # Try decrypting the command
+                print("Command before decryption:", command)
+                command = Encrypt.encrypt(keyList, command)
+                print("\n\nCommand after decryption:", command)
 
-                time.sleep(float(delay))
+                print("Received Command:", data)
+
+                if command == "stop":
+                    break
+
+                for i in range(int(exeCount)):
+                    print("Execution Number:", i)
+                    startTime = time.time()
+                    try:
+                        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+                    except subprocess.CalledProcessError as e:
+                        output = str(e).encode('Latin-1')
+                    endTime = time.time()
+                    print("Execution Time:", endTime-startTime, "second(s)")
+                    print("Execution Output:", output.decode("Latin-1"))
+                    print("\nOutput Size:", len(output), "bytes\n")
+
+                    serverTime = subprocess.check_output("date", stderr=subprocess.STDOUT, shell=True)
+                    connection.send(serverTime)
+                    
+                    # add some delay to prevent timing issues
+                    time.sleep(0.5)
+
+                    connection.send(output)
+
+                    time.sleep(float(delay))
+
     except BrokenPipeError as e:
         print("BrokenPipeError, most likely caused by rcend command on client side.\nAttempting to exit gracefully.")
         print(e, '\n\n\n')
